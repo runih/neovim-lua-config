c25e0f5f58c970b9ffec26bc3860ac4d8262897a
Rúni H.Hansen <runi.hansen@okkara.net>
2023-01-13 00:37:08 +0100

    Added all git pluins in the same file and added support for lua test

---
+++ lua/plugins/git.lua
@@ -0,0 +1,33 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  dependencies = {
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb',
+    'kdheepak/lazygit.nvim',
+    'TimUntersberger/neogit',
+    'tanvirtin/vgit.nvim',
+  },
+
+  config = function()
+    local gitsigns_ok, gitsigns = pcall(require , "gitsigns")
+    if not gitsigns_ok then
+      return
+    end
+
+    gitsigns.setup()
+
+    local neogit_ok, neogit = pcall(require, "neogit")
+    if not neogit_ok then
+      return
+    end
+
+    neogit.setup()
+
+    local vgit_ok, vgit = pcall(require, "vgit")
+    if not vgit_ok then
+      return
+    end
+
+    vgit.setup()
+  end
+}
