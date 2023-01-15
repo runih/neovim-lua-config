f433ac62da78265bd9c01e993a271816a3a682ae
Rúni H.Hansen <runi.hansen@okkara.net>
2023-01-15 12:58:53 +0100

    Changed the DAP keymappings to Lua mappings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -20,8 +20,8 @@ keymap.set("n", "<leader>cd", "<cmd>lcd %:h<CR>")
 keymap.set("n", "<leader>sm", "<cmd>MaximizerToggle<CR>")
 
 -- telescope
-local ok, builtin = pcall(require, "telescope.builtin")
-if ok then
+local builtin_loaded, builtin = pcall(require, "telescope.builtin")
+if builtin_loaded then
 
   local find_in_current_buff = function ()
     -- search in current buffer
@@ -66,13 +66,29 @@ keymap.set("n", "<leader>L", "<cmd>Lazy<CR>", { desc = "[L]azy" })
 keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "LazyGit" })
 
 -- debugging
-keymap.set("n", "<F2>", "<cmd>lua require'dap'.continue()<CR>")
-keymap.set("n", "<F3>", "<cmd>lua require'dap'.step_into()<CR>")
-keymap.set("n", "<F4>", "<cmd>lua require'dap'.step_over()<CR>")
-keymap.set("n", "<F5>", "<cmd>lua require'dap'.step_out()<CR>")
-keymap.set("n", "<leader>b", "<cmd>lua require'dap'.toggle_breakpoint()<CR>")
-keymap.set("n", "<leader>B", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
-keymap.set("n", "<leader>lp", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
-keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.repl.open()<CR>")
-keymap.set("n", "<leader>du", "<cmd>lua require'dapui'.open()<CR>")
-keymap.set("n", "<leader>dc", "<cmd>lua require'dapui'.close()<CR>")
+local dap_loaded, dap = pcall(require, "dap")
+if dap_loaded then
+
+  local breakpoint_with_condition = function ()
+    dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+  end
+
+  local breakpoint_based_on_log_point_message = function ()
+    dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+  end
+
+  keymap.set("n", "<F2>",         dap.continue,                                 { desc = "[F2] Debug continue" })
+  keymap.set("n", "<F3>",         dap.step_into,                                { desc = "[F3] Debug Step Into" })
+  keymap.set("n", "<F4>",         dap.step_over,                                { desc = "[F4] Debug Stop Over" })
+  keymap.set("n", "<F5>",         dap.step_out,                                 { desc = "[F5] Debug Stop Out" })
+  keymap.set("n", "<leader>b",    dap.toggle_breakpoint,                        { desc = "Toggle [b]reakpoint" })
+  keymap.set("n", "<leader>B",    breakpoint_with_condition,                    { desc = "Toggle [B]reakpoint with condition"})
+  keymap.set("n", "<leader>lp",   breakpoint_based_on_log_point_message,        { desc = "Toggle breakpoint based on [l]og [p]oint message" })
+  keymap.set("n", "<leader>dr",   dap.repl.open,                                { desc = "[d]ebug [r]epl open"} )
+end
+
+local dapui_loaded, dapui = pcall(require, "dapui")
+if dapui_loaded then
+  keymap.set("n", "<leader>do",   dapui.open,                                   { desc = "[d]ebug UI [o]pen" })
+  keymap.set("n", "<leader>dc",   dapui.close,                                  { desc = "[d]ebug UI [c]close" })
+end
