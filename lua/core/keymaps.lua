0157a165537067983b09e3e58bdba4774b4987f8
Rúni H.Hansen <runi.hansen@okkara.net>
2023-01-16 02:23:55 +0100

    Change the alias fn to myfunctions fn is reserved by NeoVim. Cleaning up the keymaps file

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,6 @@
-local fn_loaded, fn = pcall(require, "core.functions")
-if not fn_loaded then
+-- Loading my functions
+local myfunctions_loaded, myfunctions = pcall(require, "core.functions")
+if not myfunctions_loaded then
     print("My functions not loaded!")
     return
 end
@@ -14,8 +15,8 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- mine preferences
 keymap.set("n", "<leader>cd", "<cmd>lcd %:h<CR>")
-keymap.set("n", "<leader>so", fn.load_current_luafile, { desc = "[Lo]ading current lua file" })
-keymap.set("n", "<leader>!", fn.execute_current_line, { desc = "Execute current line in to a buffer" })
+keymap.set("n", "<leader>so", myfunctions.load_current_luafile,           { desc = "[Lo]ading current lua file" })
+keymap.set("n", "<leader>!", myfunctions.execute_current_line,            { desc = "Execute current line in to a buffer" })
 
 -- plugin keymaps
 
@@ -35,50 +36,46 @@ if builtin_loaded then
   keymap.set("n", "<leader>sd",       builtin.diagnostics,                { desc = "[S]earch [D]iagnostics" })
   keymap.set("n", "<leader>km",       builtin.keymaps,                    { desc = "[K]ey[M]aps" })
   keymap.set("n", "<leader>st",       builtin.filetypes,                  { desc = "[S]search [T]ypes" })
-  keymap.set("n", "|",                fn.find_in_current_buff,            { desc = "[|] Search in current buffer" })
-  keymap.set("n", "<leader>sn",       fn.nvim_config,                     { desc = "[S]earch [N]eoVim configurations" })
+  keymap.set("n", "|",                myfunctions.find_in_current_buff,   { desc = "[|] Search in current buffer" })
+  keymap.set("n", "<leader>sn",       myfunctions.nvim_config,            { desc = "[S]earch [N]eoVim configurations" })
 end
 
 local telescope_tabs_loaded, telescope_tabs = pcall(require, "telescope-tabs")
 if telescope_tabs_loaded then
-  keymap.set("n", "<leader>t", telescope_tabs.list_tabs,                    { desc = "List [T]abs" })
+  keymap.set("n", "<leader>t", telescope_tabs.list_tabs,                  { desc = "List [T]abs" })
 end
 
 -- For Testing lua files
-keymap.set("n", "<leader>T", "<Plug>PlenaryTestFile", { desc = "Plenary[T]estFile" })
+keymap.set("n", "<leader>T", "<Plug>PlenaryTestFile",                     { desc = "Plenary[T]estFile" })
 
 -- Alpha
-keymap.set("n", "<leader>a", "<cmd>Alpha<CR>", { desc = "[A]lpha" })
+keymap.set("n", "<leader>a", "<cmd>Alpha<CR>",                            { desc = "[A]lpha" })
 
 -- Mason
-keymap.set("n", "<leader>M", "<cmd>Mason<CR>", { desc = "[M]ason" })
+keymap.set("n", "<leader>M", "<cmd>Mason<CR>",                            { desc = "[M]ason" })
 
 -- Lazy
-keymap.set("n", "<leader>L", "<cmd>Lazy<CR>", { desc = "[L]azy" })
+keymap.set("n", "<leader>L", "<cmd>Lazy<CR>",                             { desc = "[L]azy" })
 
 -- LazyGit
-keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "LazyGit" })
+keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>",                         { desc = "LazyGit" })
 
 -- debugging
 local dap_loaded, dap = pcall(require, "dap")
 if dap_loaded then
-  keymap.set("n", "<F2>",         dap.continue,                                 { desc = "[F2] Debug continue" })
-  keymap.set("n", "<F3>",         dap.step_into,                                { desc = "[F3] Debug Step Into" })
-  keymap.set("n", "<F4>",         dap.step_over,                                { desc = "[F4] Debug Stop Over" })
-  keymap.set("n", "<F5>",         dap.step_out,                                 { desc = "[F5] Debug Stop Out" })
-  keymap.set("n", "<leader>b",    dap.toggle_breakpoint,                        { desc = "Toggle [b]reakpoint" })
-  keymap.set("n", "<leader>B",    fn.breakpoint_with_condition,                    { desc = "Toggle [B]reakpoint with condition"})
-  keymap.set("n", "<leader>lp",   fn.breakpoint_based_on_log_point_message,        { desc = "Toggle breakpoint based on [l]og [p]oint message" })
-  keymap.set("n", "<leader>dr",   dap.repl.open,                                { desc = "[d]ebug [r]epl open"} )
+  keymap.set("n", "<F2>",         dap.continue,                                             { desc = "[F2] Debug continue" })
+  keymap.set("n", "<F3>",         dap.step_into,                                            { desc = "[F3] Debug Step Into" })
+  keymap.set("n", "<F4>",         dap.step_over,                                            { desc = "[F4] Debug Stop Over" })
+  keymap.set("n", "<F5>",         dap.step_out,                                             { desc = "[F5] Debug Stop Out" })
+  keymap.set("n", "<leader>b",    dap.toggle_breakpoint,                                    { desc = "Toggle [b]reakpoint" })
+  keymap.set("n", "<leader>B",    myfunctions.breakpoint_with_condition,                    { desc = "Toggle [B]reakpoint with condition"})
+  keymap.set("n", "<leader>lp",   myfunctions.breakpoint_based_on_log_point_message,        { desc = "Toggle breakpoint based on [l]og [p]oint message" })
+  keymap.set("n", "<leader>dr",   dap.repl.open,                                            { desc = "[d]ebug [r]epl open"} )
+  local dapui_loaded, dapui = pcall(require, "dapui")
+  if dapui_loaded then
+    keymap.set("n", "<leader>do",   dapui.open,                                             { desc = "[d]ebug UI [o]pen" })
+    keymap.set("n", "<leader>dc",   dapui.close,                                            { desc = "[d]ebug UI [c]close" })
+  end
 end
 
-local dapui_loaded, dapui = pcall(require, "dapui")
-if dapui_loaded then
-  keymap.set("n", "<leader>do",   dapui.open,                                   { desc = "[d]ebug UI [o]pen" })
-  keymap.set("n", "<leader>dc",   dapui.close,                                  { desc = "[d]ebug UI [c]close" })
-end
-
-local neotree_loaded, neotree  = pcall(require, "neo-tree.command")
-if neotree_loaded then
-  keymap.set("n", "<c-e>", fn.open_neotree, { desc = "Open N[E]oTree" })
-end
+keymap.set("n", "<c-e>", myfunctions.open_neotree,                                          { desc = "Open N[E]oTree" })
