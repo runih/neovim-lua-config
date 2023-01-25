d6e833aa7df26ded9c8f14dc8e274edcfb9a5bad
Rúni H.Hansen <runi.hansen@notified.com>
2023-01-25 13:30:28 +0100

    Added some markdown plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,5 +1,11 @@
 return {
   {
+    'preservim/vim-markdown',
+    dependencies = {
+      'godlygeek/tabular',
+    },
+  },
+  {
     'iamcco/markdown-preview.nvim',
     build = 'cd app && yarn install',
     config = function ()
