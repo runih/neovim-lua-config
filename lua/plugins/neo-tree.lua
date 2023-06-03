01d0ec2cb3b5cb1c145016be0adf606ce906ca6b
Rúni H.Hansen <runi.hansen@okkara.net>
2023-06-03 15:14:23 +0200

    Improved my Neo-Tree setup

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,17 +1,77 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v2.x',
   --keys = {
-  --  { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "NeoTree" },
+  --  { '<leader>e', '<cmd>Neotree toggle<CR>', desc = 'NeoTree' },
   --},
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
   },
-  opts = {
-    filesystem = {
-      follow_current_file = true,
-      hijack_netrw_behavior = "open_current",
-    }
-  }
+  --opts = {
+  --  filesystem = {
+  --    follow_current_file = true,
+  --    hijack_netrw_behavior = 'open_current',
+  --  }
+  --},
+	config = function ()
+
+		-- If you want icons for diagnostic errors, you'll need to define them somewhere:
+		vim.fn.sign_define("DiagnosticSignError",
+			{text = " ", texthl = "DiagnosticSignError"})
+		vim.fn.sign_define("DiagnosticSignWarn",
+			{text = " ", texthl = "DiagnosticSignWarn"})
+		vim.fn.sign_define("DiagnosticSignInfo",
+			{text = " ", texthl = "DiagnosticSignInfo"})
+		vim.fn.sign_define("DiagnosticSignHint",
+			{text = "", texthl = "DiagnosticSignHint"})
+
+		local ok, neo_tree = pcall(require, 'neo-tree')
+		if not ok then
+			return
+		end
+    neo_tree.setup({
+      default_component_configs = {
+        icon = {
+          folder_empty = "󰜌",
+          folder_empty_open = "󰜌",
+        },
+        git_status = {
+          symbols = {
+            renamed   = "󰁕",
+            unstaged  = "󰄱",
+          },
+        },
+      },
+      document_symbols = {
+        kinds = {
+          File = { icon = "󰈙", hl = "Tag" },
+          Namespace = { icon = "󰌗", hl = "Include" },
+          Package = { icon = "󰏖", hl = "Label" },
+          Class = { icon = "󰌗", hl = "Include" },
+          Property = { icon = "󰆧", hl = "@property" },
+          Enum = { icon = "󰒻", hl = "@number" },
+          Function = { icon = "󰊕", hl = "Function" },
+          String = { icon = "󰀬", hl = "String" },
+          Number = { icon = "󰎠", hl = "Number" },
+          Array = { icon = "󰅪", hl = "Type" },
+          Object = { icon = "󰅩", hl = "Type" },
+          Key = { icon = "󰌋", hl = "" },
+          Struct = { icon = "󰌗", hl = "Type" },
+          Operator = { icon = "󰆕", hl = "Operator" },
+          TypeParameter = { icon = "󰊄", hl = "Type" },
+          StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+        }
+      },
+      -- Add this section only if you've configured source selector.
+      source_selector = {
+        winbar = true,
+        statusline = true,
+        sources = {
+          { source = "filesystem", display_name = " 󰉓 Files " },
+          { source = "git_status", display_name = " 󰊢 Git " },
+        },
+      },
+    })
+	end
 }
