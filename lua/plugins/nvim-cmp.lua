9fb0c1de756a2d597d7ee14bb4ca297064db3a00
Rúni H.Hansen <runi.hansen@okkara.net>
2023-07-14 17:27:11 +0200

    Added LuaSnip and friendly-snippets

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -20,7 +20,11 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
+
+    -- snippets
+    'L3MON4D3/LuaSnip',
     "saadparwaiz1/cmp_luasnip",
+    'rafamadriz/friendly-snippets',
   },
   opts = function()
     local cmp = require("cmp")
