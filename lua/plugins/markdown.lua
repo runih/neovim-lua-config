31eaf1e1118343aefc85b2f6b453893184bb0cd0
Rúni H.Hansen <runi.hansen@okkara.net>
2024-01-23 13:56:24 +0100

    Changed to nvim-markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,16 +1,12 @@
 return {
-  {
-    'preservim/vim-markdown',
-    dependencies = {
-      'godlygeek/tabular',
-    },
-  },
-  {
-    'iamcco/markdown-preview.nvim',
-    build = 'cd app && yarn install',
-    config = function ()
-      vim.g.mkdp_browser = '/Applications/Vivaldi.app/Contents/MacOS/Vivaldi'
-      vim.g.mkdp_auto_close = 0
-    end
-  }
+	{
+		"ixru/nvim-markdown",
+		"godlygeek/tabular",
+		"iamcco/markdown-preview.nvim",
+		build = "cd app && yarn install",
+		config = function()
+			vim.g.mkdp_browser = "/Applications/Vivaldi.app/Contents/MacOS/Vivaldi"
+			vim.g.mkdp_auto_close = 0
+		end,
+	},
 }
