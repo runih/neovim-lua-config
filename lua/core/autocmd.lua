a6d0da8c8c945359b658934edbea2f97a42fb3f5
Rúni H.Hansen <runi.hansen@okkara.net>
2024-02-24 23:13:28 +0100

    Added yank highlight

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,9 +1,18 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "sql",
-  callback = function()
-    vim.opt.tabstop = 4
-    vim.opt.shiftwidth = 4
-    vim.opt.expandtab = false
-    vim.bo.softtabstop = 4
-  end
+	pattern = "sql",
+	callback = function()
+		vim.opt.tabstop = 4
+		vim.opt.shiftwidth = 4
+		vim.opt.expandtab = false
+		vim.bo.softtabstop = 4
+	end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 300,
+		})
+	end,
 })
