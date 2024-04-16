73f430334a68e2c1b963641ed4dcc5a6515d0c7b
Rúni H.Hansen <runi.hansen@okkara.net>
2024-04-16 21:17:25 +0200

    Refactoring which-key plugin

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,12 +1,13 @@
 return {
-  -- Show leader mappings
   'folke/which-key.nvim',
-  config = function()
-    local ok, whichkey = pcall(require, 'which-key')
-    if not ok then
-      return
-    end
-
-    whichkey.setup()
+  event = 'VeryLazy',
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 500
   end,
+  opts = {
+    -- your configuration goes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
 }
