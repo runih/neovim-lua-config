fb72099b92c8b703d2032af303f272f61f79d3a0
Rúni H.Hansen <runi.hansen@okkara.net>
2024-04-17 02:56:10 +0200

    Updated comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,13 +1,26 @@
 return {
   -- commenting with gc
   'numToStr/Comment.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
 
   config = function()
-    local ok, comment = pcall(require, 'Comment')
-    if not ok then
+    local comment_loaded, comment = pcall(require, 'Comment')
+    if not comment_loaded then
       return
     end
 
-    comment.setup()
+    local ts_context_commentstring_loaded, ts_context_commentstring =
+      pcall(require, 'ts_context_commentstring.integrations.comment_nvim')
+    if not ts_context_commentstring_loaded then
+      return
+    end
+
+    ---@diagnostic disable-next-line: missing-fields
+    comment.setup({
+      pre_hook = ts_context_commentstring.create_pre_hook(),
+    })
   end,
 }
