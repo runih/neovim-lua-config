0f88ca54b58b000574713ab0d9bfca10a3911d81
Rúni H.Hansen <runi.hansen@okkara.net>
2024-05-25 23:52:01 +0200

    Added new colorscheme
    Added the colorscheme:
    - tokyonight
    - catppuccin

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -5,4 +5,17 @@ return {
   'savq/melange-nvim',
   'sainnhe/edge',
   'glepnir/zephyr-nvim',
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = 'night',
+    },
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+  },
 }
