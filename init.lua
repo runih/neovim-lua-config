a7431e2003a473669335538a9fcd291cff9c377b
Rúni H.Hansen <runi.hansen@okkara.net>
2024-06-07 12:27:42 +0200

    Fixed vimr issue

--- init.lua
+++ init.lua
@@ -5,6 +5,6 @@ require('core.globals')
 require('core.autocmd')
 if vim.g.neovide then
   require('core.neovide')
-elseif vim.fn.has('gui_vimr') then
+elseif vim.g.gui_vimr then
   require('core.vimr')
 end
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -41,7 +41,7 @@ return {
     }
 
     local function clock()
-      if vim.g.neovide or vim.fn.has('gui_vimr') then
+      if vim.g.neovide or vim.g.gui_vimr then
         return { 'datetime', style = '%H:%M' }
       end
     end
