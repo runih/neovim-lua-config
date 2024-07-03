d868b5f43f86fbe5db3e327860b430b375475f5f
Rúni H.Hansen <runi.hansen@okkara.net>
2024-07-03 11:45:56 +0200

    NeoVide cleanup

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -3,18 +3,8 @@ local opt = vim.opt -- for conciseness
 -- Neovide settings
 opt.guifont = { 'Hack_Nerd_Font_Mono:h17' }
 -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-vim.g.neovide_transparency = 0.5
-vim.g.transparency = 0.99
 vim.g.neovide_transparency = 0.8
 vim.g.transparency = 0.8
-local normal_highlight = vim.api.nvim_get_hl_by_name('Normal', true)
-if normal_highlight and normal_highlight.background then
-  vim.g.neovide_background_color = string.format(
-    '%06x',
-    normal_highlight.background
-  ) .. string.format('%x', (255 * vim.g.transparency))
-end
--- vim.cmd("let g:neovide_background_color = '#001828'.printf('%x', float2nr(255 * g:transparency))")
 
 -- Floating Blur Amount
 vim.g.neovide_floating_blur_amount_x = 2.0
